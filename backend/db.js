d5279b6b60142453c98ae0f3861aa1b1b90887d2
dharaneesh1881 <dharaneesh1881@gmail.com>
2025-12-16 13:51:01 +0530

    zod in signup

--- backend/db.js
+++ backend/db.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const user = new Schema({
-    name:String,
-    email:String,
-    password:String
+    name:{type:String , unique:true, required:true},
+    email:{type: String, unique:true, required:true},
+    password:{type:String,  required:true}
     
 })
 
--- backend/login.js
+++ backend/login.js
@@ -4,6 +4,7 @@ const mongoose=require('mongoose');
 const {userModel}=require('./db');
 const jwt = require('jsonwebtoken');
 const cors = require("cors");
+const {z} = require("zod");
 
 
 mongoose.connect("mongodb+srv://dharaneesh1881:Dd%409790361881@cluster0.su0jsfi.mongodb.net/tweet");
@@ -13,24 +14,48 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/signup",async function(req,res){
+    const requiredbody = z.object({
+    name: z.string().min(3, "Name too short"),
+    email: z.string().email("Invalid email"),
+    password: z.string().min(6, "Password must be 6 characters")
+    });
+
+    const parsedDatawithSuccess=requiredbody.safeParse(req.body);
+
+    if(!parsedDatawithSuccess.success){
+        res.status(400).json({
+            message:parsedDatawithSuccess.error.issues.map((e)=>e.message).join(", ")
+        });
+        return ;
+    }
 
     const name=req.body.name ; 
     const email=req.body.email;
     const password=req.body.password;
     
-    
-    const hashedpassword= await bcrypt.hash(password,5);
-    
-    await userModel.create({
-        name:name,
-        email:email,
-        password:hashedpassword
+    let error=false;
+    try{
+        const hashedpassword= await bcrypt.hash(password,5);
         
-    });
-
-    res.json({
-        message:"User signed up"
-    });
+        await userModel.create({
+            name:name,
+            email:email,
+            password:hashedpassword
+            
+        });
+    }
+    catch(e){
+        console.log(e);
+        error=true;
+        res.status(409).json({
+            message:"user already exists"
+        });
+    }
+    if(!error){
+        res.json({
+            message:"User signed up"
+        });
+    }
  
 });
 
